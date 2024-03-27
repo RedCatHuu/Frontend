7f03568e46df82371bf7567e2b8845951a067b7a
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-03-27 05:34:06 +0000

    [add]controller, model, js, scss

---
+++ app/javascript/script.js
@@ -0,0 +1,2 @@
+let title ="js"
+// alert(title)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'books#index'
+  get 'books/create'
+  get 'books/update'
+  get 'books/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
