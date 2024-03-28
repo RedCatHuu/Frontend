572d7f247af613f3f0fba1bc8a343e37561bb511
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-03-28 02:57:03 +0000

    [add]menu編集画面、削除機能。index廃止

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,5 @@
 class BooksController < ApplicationController
   
-  def index
-    @books = Book.all
-  end 
-  
   def show
     @books = Book.all
     @book = Book.find(params[:id])
@@ -16,11 +12,27 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.find(params[:id])
   end
+  
+  def menu_edit
+    @books = Book.all
+    @book = Book.find(params[:book_id])
+    # redirect_to book_path(@book)
+    render :menu_edit
+  end
 
   def update
 
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path(book)
   end
+end
+
+private
+
+def book_params
+    params.require(:book).permit(:title, :content)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'books#index'
+  root to: 'books#show', id: 1
   resources :books, only: [:show, :create, :edit, :update, :destroy]
+  get 'menu_edit' => 'books#menu_edit'
+  get 'title_edit' => 'books#title_edit'
+  get 'content_edit' => 'books#content_edit'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
